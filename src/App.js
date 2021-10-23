285afcc8c863aabcf228283050f0a805601a566e
Ahmed Hakim <me@hakim.ninja>
2021-10-23 16:49:52 +0200

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import  React from "react";
+import React from "react";
 
 import { Provider } from "react-redux";
 import Layout from "./componenets/layout";
 import { useStore } from "./store";
 import Movies from "./screens/Movies";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 export default function App() {
   const store = useStore();
-  
+
   return (
     <Provider store={store}>
-      <Layout>
-        <Movies/>
-      </Layout>
-     
+      <Router>
+        <Layout>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/fav">
+            <h1>will show fav here</h1>
+          </Route>
+        </Layout>
+      </Router>
     </Provider>
   );
 }
--- src/componenets/layout/index.js
+++ src/componenets/layout/index.js
@@ -6,21 +6,44 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
+import {Box, Paper} from '@mui/material';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import MovieIcon from '@mui/icons-material/Movie';
+import {
+  Link
+} from "react-router-dom";
 const Layout = (props) => {
   const { children } = props;
+  const [value, setValue] = React.useState(0);
   return (
     <>
-      <AppBar position="relative">
+      {/* <AppBar position="relative">
         <Toolbar>
           <CameraIcon sx={{ mr: 2 }} />
           <Typography variant="h6" color="inherit" noWrap>
             Movies
           </Typography>
         </Toolbar>
-      </AppBar>
+      </AppBar> */}
       <main>
-        <Container maxWidth="xl">{children}</Container>
+        <Container sx={{ marginTop:4, marginBottom:12 }} maxWidth="xl">{children}</Container>
       </main>
+      <Paper sx={{zIndex:1 ,position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction component={Link} to="/" label="Now Playing" icon={<MovieIcon />} />
+        <BottomNavigationAction component={Link} to="/fav"  label="Favorites" icon={<FavoriteIcon />} />
+      </BottomNavigation>
+    </Paper>
     </>
   );
 };
